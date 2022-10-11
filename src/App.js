516c5585d6b6f3835b0b842438fbc30a33e6e5c5
mustahsin2000 <mustahsin152415@gmail.com>
2022-10-11 03:36:41 -0700

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home/Home';
+import Topics from './components/Topics/Topics';
+import Main from './Layout/Main';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path:'/',
+      element:<Main></Main>,
+      children:
+    [
+      {
+        path:'/',
+        element:<Home></Home>
+      },
+      {
+        path:'/topics',
+        element:<Topics></Topics>
+      }
+    ]
+    }
+  ])
   return (
     <div className="App">
-     
+     <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
